71f59c7d6786515efd48953bae0896b7be773f8b
bxnto <benjiled@gmail.com>
2024-10-22 21:51:09 -0400

    initlal commit

---
+++ Septacular/SeptacularApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeptacularApp.swift
+//  Septacular
+//
+//  Created by Benjamin Ledbetter on 10/22/24.
+//
+
+import SwiftUI
+
+@main
+struct SeptacularApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SeptacularTests/SeptacularTests.swift
@@ -0,0 +1,17 @@
+//
+//  SeptacularTests.swift
+//  SeptacularTests
+//
+//  Created by Benjamin Ledbetter on 10/22/24.
+//
+
+import Testing
+@testable import Septacular
+
+struct SeptacularTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
